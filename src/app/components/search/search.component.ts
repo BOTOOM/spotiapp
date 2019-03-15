77838ecdaee6a5a7efd0acf44244caf6c8211674
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-03-15 01:31:20 -0500

    inicio y funcionalidad de search

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {SpotifyService} from '../../services/spotify.service';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  artistas: any[] = [];
+
+  constructor(
+    private spotify: SpotifyService,
+  ) { }
 
   ngOnInit() {
+  }
+
+  buscar( termino:string ) {
+    console.info(termino);
+    this.spotify.getArtista(termino)
+    .subscribe(data => {
+      this.artistas = <any>data['artists']['items'];
+      console.info(this.artistas);
+    });
   }
 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -6,7 +6,7 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 })
 export class SpotifyService {
 
-  token = 'BQBF6OAA-SS_tChuCgLAUC6pcZmPy8EOa4mWVTFZBWPfnHbdA5gDxcfm9YsJLeFM2tgiCKpVooZ8CU_ct5c';
+  token = 'BQAtvoeX4nyDNnnYdoxukV3fTR6oKi-veyQ_8EILXah1y8YRVFfIjxmT1f1865EwuA8TzLXQ2wdiMJfVDV0';
 
   constructor(
     private http: HttpClient,
@@ -20,7 +20,15 @@ export class SpotifyService {
       'Authorization': `Bearer ${this.token}`
     });
 
-    return this.http.get(`https://api.spotify.com/v1/browse/new-releases?limit=20`, { headers });
+    return this.http.get(`https://api.spotify.com/v1/browse/new-releases?offset=5&limit=20`, { headers });
 
+  }
+
+  getArtista( termino:string ) {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.token}`
+    });
+
+    return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers });
   }
 }
